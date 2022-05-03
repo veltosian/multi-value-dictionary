dc699379bbc80a3b2be86e99ee75c9ef04305857
veltosian <39470169+veltosian@users.noreply.github.com>
2022-05-02 18:53:29 -0700

    Get command and arguments from user input

--- index.js
+++ index.js
@@ -6,8 +6,51 @@ async function run() {
 
     while (true) {
         const input = question('>');
-        console.log(`You input ${input}`);
+        const command = getCommand(input);
+        if (!command) {
+            console.log(`Unsupported command: "${input}"`)
+            continue;
+        }
+        const {key, member} = getArguments(input);
+        handleCommand(command, key, member);
     }
+}
+
+function getCommand(input) {
+    const command = input.split(' ')[0];
+    const validCommands = [
+        'KEYS',
+        'MEMBERS',
+        'ADD',
+        'REMOVE',
+        'REMOVEALL',
+        'CLEAR',
+        'KEYEXISTS',
+        'MEMBEREXISTS',
+        'ALLMEMBERS',
+        'ITEMS'
+    ];
+
+    if (validCommands.includes(command)) {
+        return command;
+    }
+    else {
+        return null;
+    }
+}
+
+function getArguments(input) {
+    // TODO Add validation to input arguments
+    const arguments = input.split(' ');
+    const key = arguments[1];
+    const member = arguments[2];
+    return {key, member};
+}
+
+function handleCommand(command, key, member) {
+    console.log(`Command:\t${command}`);
+    console.log(`Key:\t\t${key}`)
+    console.log(`Member:\t\t${member}`)
 }
 
 run().then();
