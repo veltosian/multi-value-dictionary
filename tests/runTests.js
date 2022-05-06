843424e0bccb5e4f4e317dfa29a091876f5124b1
Zach <39470169+veltosian@users.noreply.github.com>
2022-05-05 19:27:26 -0700

    Update tests to run through dictionaryOutputHandler and also add removeTest

--- tests/runTests.js
+++ tests/runTests.js
@@ -1,17 +1,31 @@
-const Dictionary = require('../src/dictionary');
+const DictionaryOutputHandler = require('../src/dictionaryOutputHandler');
 
 function main() {
     console.log(`Add Test : ${addTest() ? 'PASS' : 'FAIL'}`);
+    console.log(`Remove Test : ${removeTest() ? 'PASS' : 'FAIL'}`);
 }
 
 function addTest() {
     let pass = true;
-    const dictionary = new Dictionary();
-    dictionary.add('foo', 'bar');
-    if (dictionary.getKeys().join('') !== 'foo') {
+    const dictionaryOutputHandler = new DictionaryOutputHandler();
+    dictionaryOutputHandler.handleCommand('ADD', 'foo', 'bar');
+    const dictionaryCopy = dictionaryOutputHandler.dictionary.getItems();
+    if (Object.keys(dictionaryCopy).join('') !== 'foo') {
         pass = false;
     }
     return pass;
+}
+
+function removeTest() {
+    const dictionaryOutputHandler = new DictionaryOutputHandler();
+    dictionaryOutputHandler.dictionary.dictionary = { 'foo': ['bar', 'baz'] };
+    dictionaryOutputHandler.handleCommand('REMOVE', 'foo', 'baz');
+
+    const dictionaryCopy = dictionaryOutputHandler.dictionary.getItems();
+    let result = true;
+    result = result && Object.keys(dictionaryCopy).join('') === 'foo';
+    result = result && dictionaryCopy.foo.length === 1 && dictionaryCopy.foo[0] === 'bar';
+    return result;
 }
 
 main();
